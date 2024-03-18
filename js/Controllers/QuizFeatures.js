b19eb56871cc026bda783f0335f66078025bcd21
Isaque dos Santos <74436802+Isaque-Claudino-dos-Santos@users.noreply.github.com>
2024-03-18 19:25:04 -0300

    Add files via upload

---
+++ js/Controllers/QuizFeatures.js
@@ -0,0 +1,35 @@
+import Feature from "./Feature.js";
+
+export default class QuizFeatures extends Feature {
+  async attempResponse() {
+    const { listAttemps, inputResponse, statistics } = this.data;
+
+    const value = this.app.inputResponseValue;
+
+    if (!value || value === "") return;
+
+    if (this.app.hobby === value) {
+      alert("Sua resposta está correta");
+      this.app.setTotalCorrectResponse(this.app.totalCorrectResponse + 1);
+      statistics.setTextTotalCorrectResoponse();
+      await this.addTipAfterRequestDataQuiz();
+    }
+
+    //Add item in list
+    this.app.addAttemp();
+    listAttemps.addItem(value);
+
+    //Clear input value
+    inputResponse.clearValue("");
+
+    //render statistics
+    statistics.setTextTotalAttemp();
+  }
+
+  async addTipAfterRequestDataQuiz() {
+    const { tip } = this.data;
+    tip.skeleton();
+    await this.app.requestDataQuiz();
+    tip.loaded();
+  }
+}
